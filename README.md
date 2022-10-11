d51679b5bea8907c8e3f51b13a423e3a550ee2cd
OnMyLevel <merilb78@gmail.com>
2022-10-11 10:12:33 -0400

    git ignore
    - node modules

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # type_test_jest
  example of test with jest
+# etape - 1 
+npm install 
+# etape - 2 
+npm run test 
--- util.test.js
+++ util.test.js
@@ -1,24 +1,40 @@
 const puppeteer = require('puppeteer');
+jest.mock('./http');
+const { generateText, checkAndGenerate, oadTitle } = require('./util');
 
-describe('END TO END TEST', () => {
+describe('Test App', () => {
 
-    test('should create an element with text and correct class', async () => {
-        const browser = await puppeteer.launch({
-          headless: true,
-           slowMo: 80,
-          args: ['--window-size=1920,1080']
-        });
-        const page = await browser.newPage();
-        await page.goto(
-          'file:///Users/christanismerilbanzouzi/Documents/GitHub/js-testing-introduction/index.html'
-        );
-        await page.click('input#name');
-        await page.type('input#name', 'Anna');
-        await page.click('input#age');
-        await page.type('input#age', '28');
-        await page.click('#btnAddUser');
-        const finalText = await page.$eval('.user-item', el => el.textContent);
-        expect(finalText).toBe('Anna (28 years old)');
-      }, 10000);
+  it('should output name and age', () => {
+      const text = generateText('Max', 29);
+      expect(text).toBe('Max (29 years old)');
+  });
+  
+  it('should output name and age', () => {
+      const text = generateText('', null);
+      expect(text).toBe(' (null years old)');
+  });
+    
+  it('should generate a valid text output', () => {
+      const text = checkAndGenerate('Max', 29);
+      expect(text).toBe('Max (29 years old)')
+  });
 
+  test('should create an element with text and correct class', async () => {
+    const browser = await puppeteer.launch({
+      headless: true,
+       slowMo: 80,
+      args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto(
+      'file:///Users/christanismerilbanzouzi/Documents/GitHub/js-testing-introduction/index.html' // specifique au poste local de l'utilisateur
+    );
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#age');
+    await page.type('input#age', '28');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+  }, 10000);
 })
